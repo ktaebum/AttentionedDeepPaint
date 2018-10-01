a600b78d540e499520dedf0dc39512c77db0db1b
ktaebum <phya.ktaebum@gmail.com>
2018-10-01 11:05:19 +0900

    change default value in learning rate and batch size. Add beta1 of adam optimizer in parser

--- utils/args.py
+++ utils/args.py
@@ -14,19 +14,25 @@ def get_default_argparser():
         help='set number of batch size',
         metavar='',
         type=int,
-        default=64)
+        default=10)
     parser.add_argument(
         '--num-epochs',
         help='set number of total epochs',
         metavar='',
         type=int,
-        default=5)
+        default=200)
     parser.add_argument(
         '--learning-rate',
         help='set training learning rate',
         metavar='',
         type=float,
-        default=1e-3)
+        default=0.0002)
+    parser.add_argument(
+        '--beta1',
+        help='set beta1 value in adam optimizer',
+        metavar='',
+        type=float,
+        default=0.5)
     parser.add_argument(
         '--verbose',
         help='set whether verbose train or not',
