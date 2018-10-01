fb37f934e160ea9e3553dd2af519a503316a7ae7
ktaebum <phya.ktaebum@gmail.com>
2018-10-01 10:03:13 +0900

    implemented niko dataset

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,1 +1,2 @@
 from preprocess.image import save_image, get_sketch
+from preprocess.process_niko import NikoPairedDataset
--- preprocess/process_niko.py
+++ preprocess/process_niko.py
@@ -1,6 +1,5 @@
 """
-Preprocess niko dataset
-and generate pair image
+Preprocess niko dataset and generate pair image
 """
 import os
 import glob
@@ -9,7 +8,69 @@ from . import get_sketch, save_image
 
 from PIL import Image
 
+import torch
+import torch.nn as nn
+
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+class NikoPairedDataset(Dataset):
+    """
+    Niko Manga Paired Dataset
+    Composed of
+    (colorized image, sketch image)
+    """
+
+    def __init__(self, root='./data/pair_niko', mode='train', transform=None):
+        if mode not in {'train', 'val', 'test'}:
+            raise ValueError('Invalid Dataset. Pick among (train, val, test)')
+
+        root = os.path.join(root, mode)
+
+        self.is_train = (mode == 'train')
+        self.transform = transform
+        self.image_files = glob.glob(os.path.join(root, '*.png'))
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        """
+        Niko Dataset Get Item
+        @param index: index
+        Returns:
+            tuple: (imageA == original, imageB == sketch)
+        """
+
+        image = Image.open(self.image_files[index])
+
+        image_width, image_height = image.size
+        imageA = image.crop((0, 0, image_width // 2, image_height))
+        imageB = image.crop((image_width // 2, 0, image_width, image_height))
+
+        # default transforms, pad if needed and center crop 512
+        width_pad = 512 - image_width // 2
+        height_pad = 512 - image_height
+
+        padding = transforms.Pad((0, height_pad, width_pad, 0),
+                                 (255, 255, 255))
+        crop = transforms.CenterCrop(512)
+
+        imageA = padding(imageA)
+        imageA = crop(imageA)
+
+        imageB = padding(imageB)
+        imageB = crop(imageB)
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return imageA, imageB
+
+
 if __name__ == "__main__":
+    # here comes generating sketch images
     img_files = glob.glob('./data/niko/**/*.jpg', recursive=True)
     for file in img_files:
         filename = file.split('/')[-1][:-4]
