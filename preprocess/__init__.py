8faeb36502db05c871f99b62400d7b31315623ad
ktaebum <phya.ktaebum@gmail.com>
2018-10-11 23:48:20 +0900

    refactored preprocess module, add scale function

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,2 +1,3 @@
 from preprocess.image import save_image, get_sketch
-from preprocess.process_niko import NikoPairedDataset
+from preprocess.image import centor_crop_tensor, re_scale, scale
+from preprocess.niko import NikoPairedDataset
