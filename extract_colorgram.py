050b1be69b0f3fcd99840c078242549ae79ad057
ktaebum <phya.ktaebum@gmail.com>
2018-11-17 01:34:57 +0900

    add extract colorgram (query all dataset and save json about color info)

---
+++ extract_colorgram.py
@@ -0,0 +1,52 @@
+"""
+For all datasets, extract top-4 color histogram and save it into json files
+"""
+
+import json
+import os
+import glob
+
+from PIL import Image
+from colorgram import colorgram as cgm
+
+out_path = './data/pair_niko/colorgram'
+if not os.path.exists(out_path):
+    os.mkdir(out_path)
+data_path = './data/pair_niko'
+
+img_files = glob.glob(os.path.join(data_path, 'train/*.png'))
+img_files += glob.glob(os.path.join(data_path, 'val/*.png'))
+
+topk = 4
+
+
+def get_rgb(colorgram_result):
+    """
+    from colorgram_result, result rgb value as tuple of (r,g,b)
+    """
+    color = colorgram_result.rgb
+    return (color.r, color.g, color.b)
+
+
+for filename in img_files:
+    image = Image.open(filename)
+    width, height = image.size
+    image = image.crop((0, 0, width // 2, height))
+
+    image_id = filename.split('/')[-1][:-4]
+    out_file = os.path.join(out_path, '%s.json' % image_id)
+    if os.path.exists(out_file):
+        # for continuation
+        print('Already processed %s' % image_id)
+        continue
+    print('processing %s...' % image_id)
+
+    try:
+        colors = cgm.extract(image, topk + 1)
+
+        color_dict = {'%d' % i: get_rgb(colors[i]) for i in range(1, topk + 1)}
+        with open(out_file, 'w') as json_file:
+            json_file.write(json.dumps(color_dict))
+    except IndexError:
+        print('Remove %s' % filename)
+        os.remove(filename)
